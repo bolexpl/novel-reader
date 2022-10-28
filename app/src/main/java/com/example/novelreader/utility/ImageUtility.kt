924c98b638cc500baea1b0e0033e7b19f46b66f2
bolexpl <are.bol@wp.pl>
2022-10-28 21:32:49 +0200

    images

--- app/src/main/java/com/example/novelreader/utility/ImageUtility.kt
+++ app/src/main/java/com/example/novelreader/utility/ImageUtility.kt
@@ -1,6 +1,9 @@
 package com.example.novelreader.utility
 
+import android.content.ContentValues
 import android.content.Context
+import android.os.Build
+import android.provider.MediaStore
 
 object ImageUtility {
 
@@ -9,6 +12,49 @@ object ImageUtility {
 
     fun saveCover(imageUrl: String, context: Context) {
         // TODO save cover
+
+
+        // Add a specific media item.
+        val resolver = context.contentResolver
+
+        // Find all audio files on the primary external storage device.
+        val imageCollection =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                MediaStore.Images.Media.getContentUri(
+                    MediaStore.VOLUME_EXTERNAL_PRIMARY
+                )
+            } else {
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+            }
+
+        // Publish a new image.
+        val imageDetails = ContentValues().apply {
+            put(MediaStore.Images.Media.DISPLAY_NAME, "cover.jpg")
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                put(MediaStore.Images.Media.IS_PENDING, 1)
+                put(MediaStore.Images.Media.RELATIVE_PATH, "NovelReader/covers")
+            }
+        }
+
+        // Keeps a handle to the new image's URI in case we need to modify it
+        // later.
+        val imageContentUri = resolver.insert(imageCollection, imageDetails)
+
+        imageContentUri?.let {
+            resolver
+                .openFileDescriptor(imageContentUri, "w", null)
+                .use { pfd ->
+                    // Write data into the pending image file.
+                }
+
+            // Now that we're finished, release the "pending" status, and allow other apps
+            // to read the image.
+            imageDetails.clear()
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                imageDetails.put(MediaStore.Audio.Media.IS_PENDING, 0)
+            }
+            resolver.update(imageContentUri, imageDetails, null, null)
+        }
     }
 
     fun saveIllustration() {
